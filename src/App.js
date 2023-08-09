b5ddb807bf2ca12583a4a19bb279811678ea3adf
Himanshu Sahu <109578416+himanshu20163@users.noreply.github.com>
2023-08-09 22:09:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import './App.css';
 import All_cards from './Components/React-Props-001/All_cards';
 import Blogcomponent from './Components/React-Props-002/Blogcomponent';
 import Allblog from './Components/React-Props-002/Allblog';
-// import Profile from './Components/React-Component-001/Profile';
+import Profile from './Components/React-Component-001/Profile';
 
 function App() {
   return (
     <div className="App">
-      {/* <Profile /> */}
+      <Profile />
       {/* <All_cards /> */}
-      <Allblog />
+      {/* <Allblog /> */ }
     </div>
   );
 }
